b3d7f46a14ef7cc1e38736dc835829a0efbba47b
rogalskim <mateusz.rogalski.88@gmail.com>
2021-02-09 16:59:58 +0100

    Explore the nlohmann/json library using sample files
    This quick draft is just a work-in-progress on a function for finding
    and modifying certain objects within a given JSON, using the
    nlohmann/json lib.

--- main.cpp
+++ main.cpp
@@ -1,18 +1,83 @@
+#include <cassert>
 #include <iostream>
+#include <filesystem>
+#include <fstream>
+#include <stdexcept>
 #include <string>
+#include <vector>
 
 #include "nlohmann_json/json.hpp"
 
+using Json = nlohmann::json;
+namespace fs = std::filesystem;
+
+Json readJsonFromDisk(const fs::path& path)
+{
+  if (!fs::exists(path))
+    throw std::invalid_argument("File doesn't exist");
+  if (path.extension() != ".json")
+    throw std::invalid_argument("File is not of .json type");
+
+  std::ifstream jsonFile(path, std::ifstream::in);
+
+  if (!jsonFile.good())
+    throw std::runtime_error("Failed to open file " + path.string());
+
+  Json json;
+  jsonFile >> json;
+
+  jsonFile.close();
+
+  return json;
+}
+
+//std::vector<const Json&> findNerLabels(const Json& labelsArray)
+//{
+//  assert(labelsArray.is_array());
+
+//  return std::vector<const Json&>();
+//}
+
+//Json createPolemLabels(std::vector<const Json&> nerLabels)
+//{
+//  return Json();
+//}
+
 int main()
 {
-  nlohmann::json j;
-  j["Hello!"] = "World";
-  j["Year"] = 2021;
-  j["NCC"] = {1,7,0,1};
+  auto json = readJsonFromDisk("../data/test_input.json");
 
-  std::string serialized = j.dump();
+  const std::string docsKey = "docs";
+  const std::string labelsKey = "labels";
 
-  std::cout << serialized << "\n";
+  if (!json.contains(docsKey))
+  {
+    std::cerr << "Input JSON doesn't contain \"" + docsKey + "\" key; exitting.\n";
+    return 0;
+  }
+
+  const auto& docs = json.at(docsKey);
+  assert(docs.is_array());
+
+  if (docs.empty())
+  {
+    std::cerr << "\"" + docsKey + "\" item is empty; exitting.\n";
+    return 0;
+  }
+
+  std::vector<Json> labels;
+  for (const auto& doc : docs.items())
+  {
+    if (doc.value().is_object() && doc.value().contains(labelsKey))
+    {
+      const auto& labels = doc.value().at(labelsKey);
+      for (const auto& label : labels.items())
+        std::cout << label.value().dump() << "\n\n";
+      //auto nerLabels = findNerLabels(labels);
+      //auto polemLabels = createPolemLabels(nerLabels);
+      //add polemLabels to doc
+    }
+  }
 
   return 0;
 }
